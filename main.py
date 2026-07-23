d932fd5dfe791a3285bc8dca5b83a34a9b0d96b4
tumanyanzaruhi1-tech <tumanyanzaruhi1@gmail.com>
2026-07-23 18:08:03 +0400

    Improve student manager with input validation

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 users = []
-
-for i in range(3):
+count = int(input("How many students do you want to add?"))
+for i in range(count):
     name = input("Enter student name: ")
-    age = int(input("Enter age: "))
+    while True:
+        try:
+            age = int(input("Enter age: "))
+            break
+        except ValueError:
+            print("Please enter a valid age.")
 
     user = {
-        "name": name,
-        "age": age
+            "name":name,
+            "age":age
     }
-
     users.append(user)
 
 print("\nStudents older than 18:")
 
 for user in users:
     if user["age"] > 18:
-        print(f'{user["name"]}, {user["age"]}')
+        print(f"{user['name']}, {user['age']}")
+        
+
